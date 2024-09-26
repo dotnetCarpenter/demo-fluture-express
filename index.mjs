51398b7d389dafa1302361f79f66d70ed75a4277
dotnetCarpenter <jon.ronnenberg@gmail.com>
2024-09-26 18:32:17 +0200

    esm version with mjs files

--- index.mjs
+++ index.mjs
@@ -8,8 +8,8 @@ const dispatch = dispatcher (path.resolve (import.meta.dirname, "actions"))
 app.set ("view engine", "ejs")
 app.set ("views", path.join (import.meta.dirname, "views"))
 
-app.use (dispatch ("session.js"))
-app.get ("/", dispatch ("welcome.js"))
+app.use (dispatch ("session.mjs"))
+app.get ("/", dispatch ("welcome.mjs"))
 // app.get ("/json", dispatch ("welcomeJson"))
 // app.get ("/image", dispatch ("image"))
 
